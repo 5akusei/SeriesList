e03d3001ccd986cff71d99b06efb00cbc70423d3
Jeison <jeisonmulti@gmail.com>
2022-03-28 17:09:06 -0600

    Update fontawesome CDN

--- src/SeriesItem.js
+++ src/SeriesItem.js
@@ -4,7 +4,12 @@ import './SeriesItem.css'
 function SeriesItem(props) {
     return (
         <li className="SeriesItem">
-            <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>C</span>
+            <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>
+                {props.completed 
+                    ? <i class="fa fa-check-circle-o" aria-hidden="true"></i>
+                    : <i class="fa fa-circle-o" aria-hidden="true"></i>
+                }
+            </span>
             <p className={`SeriesItem-p ${props.completed && 'SeriesItem-p--complete'}`}>{props.text}</p>
             <span className="Icon Icon-delete">X</span>
         </li>
